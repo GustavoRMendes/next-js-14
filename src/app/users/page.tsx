0d265bd3a2c7a7f8725f7c3405468dfe68051b31
Gustavo Mendes <gustavormendess@gmail.com>
2024-02-23 17:40:18 -0400

    Inicialização do NextJS

---
+++ src/app/users/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const Page = () => {
+  return (
+    <>
+      <Link href='/'> Página Inicial </Link>
+      <h1>Página dos Usuários</h1>
+    </>
+  )
+}
+
+export default Page;
