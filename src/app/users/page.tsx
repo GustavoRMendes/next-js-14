7e7f78a4a27fef3396ad52f3abc0f5aa10a20b50
Gustavo Mendes <gustavormendess@gmail.com>
2024-02-24 23:37:30 -0400

    Descrição do user

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -4,7 +4,11 @@ const Page = () => {
   return (
     <>
       <Link href='/'> Página Inicial </Link>
-      <h1>Página dos Usuários</h1>
+      <h1>Gustavo Mendes</h1>
+      <p>Sou estudante de sistemas de informação. Amo ajudar pessoas, resolver problemas e principalmente programar.
+        Comecei meio tarde a estudar programação, porém minha paixão já é enorme.
+      </p>
+
     </>
   )
 }
