cd52dcc0a1a4c84d7e0d771a003477573da095f7
Gustavo Mendes <gustavormendess@gmail.com>
2024-02-28 11:21:43 -0400

    useRouter.push(/)

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -2,14 +2,21 @@
 import React from "react"
 import { usePathname } from "next/navigation";
 import { useState } from "react"
+import { useRouter } from "next/navigation";
 export default function InputForm() {
   const [inputValue,setInputValue] = useState("")
   const pathname = usePathname()
+  const router = useRouter()
   return (
     <>
       <p>
         Caminho Atual: {pathname}
       </p>
+      <nav>
+        <button onClick={() => router.push('/')} type="button">
+          Voltar para o Início
+        </button>
+      </nav>
       <form>
         <label>E-mail:</label>
         <input type="email" value={inputValue} onChange={(ev) => setInputValue(ev.target.value)} />
