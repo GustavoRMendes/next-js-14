862d03e90ad53a86ae5e0765bed216637374a82e
Gustavo Mendes <gustavormendess@gmail.com>
2024-02-29 19:29:25 -0400

    prefetch in Link from next/link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,9 +5,9 @@ export default function Header () {
   return (
     <>
       <nav style={{ display: 'flex', gap: '2rem',fontSize:'25px',padding: '2rem' }}>
-        <Link href='/users'>User</Link>
-        <Link href='/'>Home</Link>
-        <Link href='/notes'> Notes </Link>
+        <Link href='/users' prefetch={true}>User</Link>
+        <Link href='/' prefetch={true}>Home</Link>
+        <Link href='/notes' prefetch={true}> Notes </Link>
       </nav>
     </>
   )
