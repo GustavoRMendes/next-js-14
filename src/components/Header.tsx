66e82865fc8ba75b10c917cc4ad49f3019d45dcf
Gustavo Mendes <gustavormendess@gmail.com>
2024-02-29 19:30:13 -0400

    scroll in Link from next/link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,9 +5,9 @@ export default function Header () {
   return (
     <>
       <nav style={{ display: 'flex', gap: '2rem',fontSize:'25px',padding: '2rem' }}>
-        <Link href='/users' prefetch={true}>User</Link>
-        <Link href='/' prefetch={true}>Home</Link>
-        <Link href='/notes' prefetch={true}> Notes </Link>
+        <Link href='/users' prefetch={true} scroll={true}>User</Link>
+        <Link href='/' prefetch={true} scroll={true}>Home</Link>
+        <Link href='/notes' prefetch={true} scroll={true}> Notes </Link>
       </nav>
     </>
   )
